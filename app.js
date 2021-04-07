3c5a4ca29eb9b3d2cc0fb2a0f5c560b4807e1855
MHirano <michelle@hiranohomes.com>
2021-04-06 18:23:58 -0700

    section 2

--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = () => 'Name: Jane, Github: janehub';
+console.log(generatePage());
