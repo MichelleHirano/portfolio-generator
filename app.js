439ab0e3a0125f5b8fb952be8e5f505839525d50
MHirano <michelle@hiranohomes.com>
2021-04-08 17:23:04 -0700

    lesson4

--- app.js
+++ app.js
@@ -51,16 +51,16 @@ const promptUser = () => {
   };
 
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
+ const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
+ fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+  console.log('Portfolio complete! Check out index.html to see the output!');
+ });
 
 const promptProject = portfolioData => {
     // If there's no 'projects' array property, create one
@@ -149,5 +149,11 @@ if (!portfolioData.projects) {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage();
+
+    // fs.writeFile('./index.html', pageHTML, err => {
+    //   if (err) throw new Error(err);
+
+    //   console.log('Page created! Check out index.html in this directory to see it!');
+    // });
   });
